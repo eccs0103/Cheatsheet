097638737677228e7660b6178543d6296899f6eb
eccs0103 <eccs0103@gmail.com>
2022-12-21 20:43:59 +0400

    1.0.0

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,7 @@
 try {
 	function configureSheets() {
-		var templateSheetTemplate = (/** @type {HTMLTemplateElement} */ (document.querySelector(`template#sheet-template`)));
-		var container = (() => {
+		const templateSheetTemplate = (/** @type {HTMLTemplateElement} */ (document.querySelector(`template#sheet-template`)));
+		const container = (() => {
 			if (templateSheetTemplate.parentElement) {
 				return templateSheetTemplate.parentElement;
 			} else {
@@ -9,49 +9,54 @@ try {
 			}
 		})();
 		[...container.querySelectorAll(`*:not(template#sheet-template)`)].forEach((element) => element.remove());
-		for (var sheet of archiveSheets.data.map((notation) => Sheet.import(notation))) {
-			var buttonSheetInformation = container.appendChild((/** @type {HTMLButtonElement} */ ((/** @type {DocumentFragment} */ (templateSheetTemplate.content.cloneNode(true))).querySelector(`button#sheet-information`))));
-			buttonSheetInformation.addEventListener(`click`, (event) => {
-				archivePreview.data = Sheet.export(sheet);
-				location.href = `./search-engine.html`;
-			});
+		archiveSheets.data.map((notation) => Sheet.import(notation)).forEach((sheet, index) => {
+			const divSheetCell = container.appendChild((/** @type {HTMLButtonElement} */ ((/** @type {DocumentFragment} */ (templateSheetTemplate.content.cloneNode(true))).querySelector(`div#sheet-cell`))));
 			{
-				var spanSheetTitle = (/** @type {HTMLSpanElement} */ (buttonSheetInformation.querySelector(`span#sheet-title`)));
-				spanSheetTitle.innerText = sheet.title;
-				var dfnSheetDate = (/** @type {HTMLElement} */ (buttonSheetInformation.querySelector(`dfn#sheet-date`)));
-				dfnSheetDate.innerText = sheet.date.toLocaleString();
+				const buttonSheetInformation = (/** @type {HTMLButtonElement} */ (divSheetCell.querySelector(`button#sheet-information`)));
+				buttonSheetInformation.addEventListener(`click`, (event) => {
+					archivePreview.data = Sheet.export(sheet);
+					location.href = `./search-engine.html`;
+				});
+				{
+					const spanSheetTitle = (/** @type {HTMLSpanElement} */ (buttonSheetInformation.querySelector(`span#sheet-title`)));
+					spanSheetTitle.innerText = sheet.title;
+					const dfnSheetDate = (/** @type {HTMLElement} */ (buttonSheetInformation.querySelector(`dfn#sheet-date`)));
+					dfnSheetDate.innerText = sheet.date.toLocaleString();
+				}
+				const buttonRemoveSheet = (/** @type {HTMLButtonElement} */ (divSheetCell.querySelector(`button#remove-sheet`)));
+				buttonRemoveSheet.addEventListener(`click`, (event) => {
+					if (window.confirm(`Sheet '${sheet.title}' cant be restored. Are you sure to delete it?`)) {
+						archiveSheets.change((sheets) => sheets.filter((sheet, index2) => index2 != index));
+						configureSheets();
+					}
+				});
 			}
-		}
+		});
 	}
 
 	configureSheets();
 
-	var buttonImportSheet = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#import-sheet`)));
+	const buttonImportSheet = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#import-sheet`)));
 	buttonImportSheet.addEventListener(`click`, (event) => {
-		var url = window.prompt(`Enter the url.`);
-		if (url != null) {
-			if (url == `/storage -clear`) {
-				archiveSheets.data = [];
-				configureSheets();
-			} else {
-				Manager.queryText(url).then((text) => {
-					archiveSheets.change((sheets) => {
-						sheets.push(Sheet.parse(JSON.parse(text)));
-						return sheets;
-					});
-					configureSheets();
-					return null;
-				}).catch((error) => {
-					if (safeMode) {
-						if (error instanceof Error) {
-							window.alert(`'${error.name}' detected - ${error.message}\n${error.stack ?? ``}`);
-						} else {
-							window.alert(`Invalid exception type.`);
-						}
-					}
-					console.error(error);
+		const input = window.prompt(`Enter the url.`);
+		if (input != null) {
+			Manager.queryText(input).then((text) => {
+				archiveSheets.change((sheets) => {
+					sheets.push(Sheet.parse(JSON.parse(text)));
+					return sheets;
 				});
-			}
+				configureSheets();
+				return null;
+			}).catch((error) => {
+				if (safeMode) {
+					if (error instanceof Error) {
+						window.alert(`'${error.name}' detected - ${error.message}\n${error.stack ?? ``}`);
+					} else {
+						window.alert(`Invalid exception type.`);
+					}
+				}
+				console.error(error);
+			});
 		}
 	});
 } catch (error) {
