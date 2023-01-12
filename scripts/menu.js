697bc1b89b58885ce6e4fbffb573fe16dddeb255
eccs0103 <eccs0103@gmail.com>
2023-01-12 22:17:27 +0400

    1.1.7

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,19 +9,37 @@ try {
 			return item;
 		}
 	}));
+
+	const templateSheetTemplate = (/** @type {HTMLTemplateElement} */ (document.querySelector(`template#sheet-template`)));
+	const container = (() => {
+		if (templateSheetTemplate.parentElement) {
+			return templateSheetTemplate.parentElement;
+		} else {
+			throw new ReferenceError(`Parent element cant be found.`);
+		}
+	})();
+
+	const selection = (/** @type {Set<{ index: Number, sheet: Sheet }>} */ (new Set()));
+	const buttonOpenMoreDialog = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#open-more-dialog`)));
+
 	function configureSheets() {
-		const templateSheetTemplate = (/** @type {HTMLTemplateElement} */ (document.querySelector(`template#sheet-template`)));
-		const container = (() => {
-			if (templateSheetTemplate.parentElement) {
-				return templateSheetTemplate.parentElement;
-			} else {
-				throw new ReferenceError(`Parent element cant be found.`);
-			}
-		})();
 		Array.from(container.querySelectorAll(`*:not(template#sheet-template)`)).forEach((element) => element.remove());
 		archiveSheets.data.map(({ date, sheet }) => ({ date: new Date(date), sheet: Sheet.import(sheet) })).forEach(({ date, sheet }, index) => {
 			const divSheetCell = container.appendChild((/** @type {HTMLButtonElement} */ ((/** @type {DocumentFragment} */ (templateSheetTemplate.content.cloneNode(true))).querySelector(`div#sheet-cell`))));
 			{
+				const inputSheetMark = (/** @type {HTMLInputElement} */ (divSheetCell.querySelector(`input#sheet-mark`)));
+				inputSheetMark.id = `sheet-mark-${index}`;
+				const data = { index: index, sheet: sheet };
+				inputSheetMark.addEventListener(`change`, (event) => {
+					if (inputSheetMark.checked) {
+						selection.add(data);
+					} else {
+						selection.delete(data);
+					}
+					buttonOpenMoreDialog.hidden = !(selection.size > 0);
+				});
+				const labelSheetMark = (/** @type {HTMLLabelElement} */ (divSheetCell.querySelector(`label[for="sheet-mark"]`)));
+				labelSheetMark.htmlFor = inputSheetMark.id;
 				const buttonSheetInformation = (/** @type {HTMLButtonElement} */ (divSheetCell.querySelector(`button#sheet-information`)));
 				buttonSheetInformation.addEventListener(`click`, (event) => {
 					archivePreview.data = Sheet.export(sheet);
@@ -31,13 +49,13 @@ try {
 				spanSheetTitle.innerText = sheet.title;
 				const dfnSheetDate = (/** @type {HTMLElement} */ (divSheetCell.querySelector(`dfn#sheet-date`)));
 				dfnSheetDate.innerText = date.toLocaleString();
-				const buttonRemoveSheet = (/** @type {HTMLButtonElement} */ (divSheetCell.querySelector(`button#remove-sheet`)));
-				buttonRemoveSheet.addEventListener(`click`, (event) => {
-					if (window.confirm(`Sheet '${sheet.title}' cant be restored. Are you sure to delete it?`)) {
-						archiveSheets.change((sheets) => sheets.filter((sheet, index2) => index2 != index));
-						configureSheets();
-					}
-				});
+				// const buttonRemoveSheet = (/** @type {HTMLButtonElement} */ (divSheetCell.querySelector(`button#remove-sheet`)));
+				// buttonRemoveSheet.addEventListener(`click`, (event) => {
+				// 	if (window.confirm(`Sheet '${sheet.title}' cant be restored. Are you sure to delete it?`)) {
+				// 		archiveSheets.change((sheets) => sheets.filter((sheet, index2) => index2 != index));
+				// 		configureSheets();
+				// 	}
+				// });
 			}
 		});
 	}
@@ -46,7 +64,7 @@ try {
 
 	const buttonOpenInsertDialog = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#open-insert-dialog`)));
 	const dialogInsertSheet = (/** @type {HTMLDialogElement} */ (document.querySelector(`dialog#insert-sheet`)));
-	buttonOpenInsertDialog.addEventListener(`click`, async (event) => {
+	buttonOpenInsertDialog.addEventListener(`click`, (event) => {
 		dialogInsertSheet.showModal();
 		dialogInsertSheet.addEventListener(`click`, (event2) => {
 			if (event2.target == dialogInsertSheet) {
@@ -60,16 +78,16 @@ try {
 		try {
 			const files = inputUploadSheet.files;
 			if (files) {
-				const file = files[0];
-				const text = await file.text()
-					.then((value) => value)
-					.catch((reason) => {
-						throw (reason instanceof Error ? reason : new Error(reason));
-					});
-				archiveSheets.change((sheets) => {
-					sheets.push({ date: Date.now(), sheet: Sheet.export(Sheet.import(JSON.parse(text))) });
-					return sheets;
-				});
+				const results = [];
+				for (const file of files) {
+					const text = await file.text()
+						.then((value) => value)
+						.catch((reason) => {
+							throw (reason instanceof Error ? reason : new Error(reason));
+						});
+					results.push({ date: Date.now(), sheet: Sheet.export(Sheet.import(JSON.parse(text))) });
+				}
+				archiveSheets.change((sheets) => [...sheets, ...results]);
 				configureSheets();
 				dialogInsertSheet.close();
 			}
@@ -109,6 +127,43 @@ try {
 				location.reload();
 			} else console.error(error);
 		}
+	});
+
+	const dialogMoreActions = (/** @type {HTMLDialogElement} */ (document.querySelector(`dialog#more-actions`)));
+	buttonOpenMoreDialog.addEventListener(`click`, (event) => {
+		dialogMoreActions.showModal();
+		dialogMoreActions.addEventListener(`click`, (event2) => {
+			if (event2.target == dialogMoreActions) {
+				dialogMoreActions.close();
+			}
+		});
+	});
+
+	const buttonDownloadSheets = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#download-sheets`)));
+	buttonDownloadSheets.addEventListener(`click`, (event) => {
+		selection.forEach((data) => {
+			Manager.download(new File([JSON.stringify(Sheet.export(data.sheet))], `${data.sheet.title}.json`, {
+				type: `text/json`,
+			}));
+		});
+		(/** @type {Array<HTMLInputElement>} */ (Array.from(container.querySelectorAll(`input[type="checkbox"][id^="sheet-mark"]`)))).forEach((element) => {
+			element.checked = false;
+		});
+		buttonOpenMoreDialog.hidden = true;
+		selection.clear();
+		dialogMoreActions.close();
+	});
+
+	const buttonDeleteSheets = (/** @type {HTMLButtonElement} */ (document.querySelector(`button#delete-sheets`)));
+	buttonDeleteSheets.addEventListener(`click`, (event) => {
+		if (window.confirm(`Sheets '${Array.from(selection).map((data) => data.sheet.title).join(`', '`)}' cant be restored. Are you sure to delete them?`)) {
+			const indexes = Array.from(selection).map((data) => data.index);
+			archiveSheets.change((data) => data.filter((sheet, index) => !indexes.includes(index)));
+			configureSheets();
+			buttonOpenMoreDialog.hidden = true;
+			selection.clear();
+		}
+		dialogMoreActions.close();
 	});
 } catch (error) {
 	if (safeMode) {
